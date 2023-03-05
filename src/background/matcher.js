66e4531bd6a15e7c03863d1cfe853c75d8ecd098
Jared Dantis <jareddantis@gmail.com>
2023-03-05 22:57:52 +0800

    background: Don't specify API URL as variable

--- src/background/matcher.js
+++ src/background/matcher.js
@@ -1,16 +1,13 @@
-import API_BASE_URL from './_constants';
-
 // Listen to detection requests from content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('[matcher] Received message from tab', sender.tab.id, 'with action:', request.action);
-  console.log('API_BASE_URL', API_BASE_URL);
 
   if (request.action === "detection") {
     const cookieBannerText = request.args.body;
     console.log('[matcher] Detection request received from tab', sender.tab.id, 'with body:', cookieBannerText);
     
     // POST to API
-    const detectionResult = fetch(API_BASE_URL + '/classify/text', {
+    const detectionResult = fetch('https://ariadne.dantis.me/api/v1/classify/text', {
       method: 'POST',
       body: JSON.stringify({
         text: cookieBannerText
--- src/background/visualmatcher.js
+++ src/background/visualmatcher.js
@@ -1,16 +1,13 @@
-import API_BASE_URL from './_constants';
-
 // Listen to visual detection requests from content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('[visualmatcher] Received message from tab', sender.tab.id, 'with action:', request.action);
-  console.log('API_BASE_URL', API_BASE_URL);
 
   if (request.action === "visualDetection") {
     const imageData = request.args.screenshot;
     console.log('[visualmatcher] Detection request received from tab', sender.tab.id, 'with image data:', imageData);
     
     // POST to API
-    const detectionResult = fetch(API_BASE_URL + '/classify/image', {
+    const detectionResult = fetch('https://ariadne.dantis.me/api/v1/classify/image', {
       method: 'POST',
       body: JSON.stringify({
         text: imageData
