664053242012255fa8b62c2b7b023d5d0b56cf7f
Jared Dantis <jareddantis@gmail.com>
2023-03-11 22:47:35 +0800

    popup/views: FalseNegative: Switch to Options API

--- src/popup/src/views/FalseNegativeView.vue
+++ src/popup/src/views/FalseNegativeView.vue
@@ -1,32 +1,6 @@
-<script setup>
-import { ref } from 'vue'
-import BigButton from '@/components/BigButton.vue'
-import Checkbox from '@/components/Checkbox.vue'
-
-const reasons = [
-  'Unclear language on cookie banner',
-  'Weighted options',
-  'Pre-filled options',
-  'Others'
-]
-
-const selectedReasons = ref([])
-
-const handleReasonChange = (args) => {
-  console.log('Called handleReasonChange', { args })
-  if (args.checked) {
-    selectedReasons.value.push(args.reason)
-  } else {
-    selectedReasons.value = selectedReasons.value.filter(
-      (r) => r !== args.reason
-    )
-  }
-}
-</script>
-
 <template>
   <main>
-    <h1 class="text-2xl text-white ml-8">Select reasons</h1>
+    <h1 class="text-2xl text-white">Select reasons</h1>
 
     <!-- Types of deceptive design -->
     <div class="mt-4">
@@ -45,17 +19,24 @@ const handleReasonChange = (args) => {
           <textarea
             class="w-full h-24 p-2 box-border rounded-lg bg-gray-100 dark:bg-gray-700 dark:text-white"
             placeholder="Please specify"
+            v-model="customReason"
+            ref="customReasonArea"
           ></textarea>
         </div>
       </div>
 
       <!-- Form controls -->
-      <div class="mt-4">
+      <div class="mt-4 grid gap-4 grid-rows-1 grid-cols-2">
         <!-- Submission button -->
         <BigButton
-          :disabled="selectedReasons.length === 0"
-          @click="() => console.log('Submit')">
+          :disabled="selectedReasons.length === 0 || (selectedReasons.includes('Others') && customReason.length === 0)"
+          @click="submitReport">
           Submit
+        </BigButton>
+
+        <!-- Cancel button -->
+        <BigButton @click="submitReport">
+          Cancel
         </BigButton>
       </div>
     </div>
@@ -64,9 +45,61 @@ const handleReasonChange = (args) => {
 
 <script>
 import { defineComponent } from 'vue'
+import { useAriadneStore } from '@/stores/ariadne';
+import BigButton from '@/components/BigButton.vue'
+import Checkbox from '@/components/Checkbox.vue'
 
 export default defineComponent({
-  name: 'FalseNegativeView'
+  name: 'FalseNegativeView',
+  components: {
+    BigButton,
+    Checkbox
+  },
+  data() {
+    return {
+      selectedReasons: [],
+      customReason: '',
+    }
+  },
+  setup() {
+    return {
+      store: useAriadneStore(),
+      reasons: [
+        'Unclear language on cookie banner',
+        'Weighted options',
+        'Pre-filled options',
+        'Others'
+      ]
+    }
+  },
+  methods: {
+    handleReasonChange(args) {
+      if (args.checked) {
+        this.selectedReasons.push(args.reason)
+
+        // If "Others" is selected, clear the custom reason and focus the textarea
+        if (args.reason === 'Others') {
+          this.customReason = ''
+          setTimeout(() => {
+            this.$refs.customReasonArea.focus()
+          }, 100)
+        }
+      } else {
+        this.selectedReasons = this.selectedReasons.filter(
+          (r) => r !== args.reason
+        )
+      }
+    },
+    submitReport(e) {
+      e.preventDefault()
+      console.log('Submitting report', {
+        selectedReasons: this.selectedReasons,
+        customReason: this.customReason,
+        url: this.store.url,
+        isRunningInExtension: this.store.isRunningInExtension
+      })
+    }
+  }
 })
 </script>
 
