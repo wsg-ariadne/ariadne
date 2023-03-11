7e9b5c8951db230ba584b86c1d47bd2c55b81aa7
Jared Dantis <jareddantis@gmail.com>
2023-03-11 22:23:46 +0800

    popup/components: Change BigButton text color while active

--- src/popup/src/components/BigButton.vue
+++ src/popup/src/components/BigButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button class="rounded-lg box-border border-2 border-adn-cyan font-bold px-2 py-4
         text-lg text-adn-indigo-s bg-adn-cyan
-        hover:bg-adn-cyan-s hover:text-white focus:text-adn-indigo-s
+        hover:bg-adn-cyan-s hover:text-white active:text-adn-indigo-s
         disabled:bg-transparent disabled:border-adn-cyan-s disabled:cursor-not-allowed
         disabled:text-adn-cyan-s">
         <slot></slot>
