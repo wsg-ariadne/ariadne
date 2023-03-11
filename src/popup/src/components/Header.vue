c199cf5195016f06f8c1a7bab6fd8a1e10615827
Jared Dantis <jareddantis@gmail.com>
2023-03-11 20:40:11 +0800

    popup/components: Taller and cleaner header layout

--- src/popup/src/components/Header.vue
+++ src/popup/src/components/Header.vue
@@ -1,22 +1,32 @@
 <script setup>
-import { RouterLink } from 'vue-router'
 import { ArrowLeftIcon } from '@heroicons/vue/24/solid'
 </script>
 
 <template>
-  <!-- Header for root path -->
-  <header v-if="$route.path === '/'" class="flex justify-center items-center">
+  <header class="flex items-center bg-adn-indigo h-12"
+    :class="{
+      'justify-center': $route.path === '/',
+      'justify-between p-2': $route.path !== '/'
+    }">
     <!-- App logo -->
-    <img class="h-4 m-auto dark:brightness-0 dark:invert" src="@/assets/logotype.svg" alt="ariadne" />
-  </header>
+    <img v-show="$route.path === '/'"
+    class="h-4 m-auto dark:brightness-0 dark:invert"
+    src="@/assets/logotype.svg"
+    alt="ariadne"
+    />
 
-  <!-- Header for non-root paths -->
-  <header v-else class="flex p-2 justify-between items-center cursor-pointer">
     <!-- Back icon -->
-    <ArrowLeftIcon @click="goBack" class="h-4 w-4 m-2 flex-none text-white" />
+    <ArrowLeftIcon v-show="$route.path !== '/'"
+    @click="goBack"
+    class="h-6 w-6 p-1 m-2 flex-none text-white cursor-pointer"
+    />
     
     <!-- Page title -->
-    <h1 class="flex-1 items-center ml-2 text-white align-middle text-lg font-bold">{{ $route.meta.title }}</h1>
+    <h1 v-show="$route.path !== '/'"
+    class="flex-1 items-center ml-2 text-white align-middle text-lg font-bold"
+    >
+      {{ $route.meta.title }}
+    </h1>
   </header>
 </template>
 
@@ -33,10 +43,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style scoped>
-header {
-  background: var(--color-background);
-  height: 2.5rem;
-}
-</style>
