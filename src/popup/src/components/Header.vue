72fcd2a4afb8ea15d0ad732f37d1ee9954ef3c96
Jared Dantis <jareddantis@gmail.com>
2023-03-11 20:10:02 +0800

    popup: Go back in history instead of linking to root

--- src/popup/src/components/Header.vue
+++ src/popup/src/components/Header.vue
@@ -11,16 +11,28 @@ import { ArrowLeftIcon } from '@heroicons/vue/24/solid'
   </header>
 
   <!-- Header for non-root paths -->
-  <header v-else class="flex p-2 justify-between items-center">
+  <header v-else class="flex p-2 justify-between items-center cursor-pointer">
     <!-- Back icon -->
-    <RouterLink to="/" class="flex-none">
-      <ArrowLeftIcon class="h-4 w-4 m-2 text-white" />
-    </RouterLink>
+    <ArrowLeftIcon @click="goBack" class="h-4 w-4 m-2 flex-none text-white" />
     
     <!-- Page title -->
     <h1 class="flex-1 items-center ml-2 text-white align-middle text-lg font-bold">{{ $route.meta.title }}</h1>
   </header>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header',
+
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
+  }
+})
+</script>
 
 <style scoped>
 header {
