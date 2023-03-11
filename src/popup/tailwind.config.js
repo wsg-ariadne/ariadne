5283f91b145a610c776d875ce36997559504c0ff
Jared Dantis <jareddantis@gmail.com>
2023-03-11 20:38:47 +0800

    popup: Extend default Tailwind config with colors

--- src/popup/tailwind.config.js
+++ src/popup/tailwind.config.js
@@ -5,13 +5,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'adn-indigo': '#18134C',
-      'adn-indigo-s': '#110D35',
-      'adn-purple': '#8200FF',
-      'adn-purple-s': '#470C85',
-      'adn-cyan': '#06CDFF',
-      'adn-cyan-s': '#0A6C8A',
+    extend: {
+      colors: {
+        'adn-indigo': '#18134C',
+        'adn-indigo-s': '#110D35',
+        'adn-purple': '#8200FF',
+        'adn-purple-s': '#470C85',
+        'adn-cyan': '#06CDFF',
+        'adn-cyan-s': '#0A6C8A',
+      },
     },
   },
   plugins: [],
