12c2dfa7620fa08743afcb1d11616dc0c61652a3
Jared Dantis <jareddantis@gmail.com>
2023-04-22 00:35:48 +0800

    vite: Auto-determine manifest version to emit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import * as fs from 'node:fs'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
+
+const outDir = process.env.MANIFEST_V3 == 'true' ? './dist-v3/' : './dist/'
+
+const copyManifest = () => {
+  return {
+    name: 'copy-manifest',
+    buildStart() {
+      if (fs.existsSync(outDir)) {
+        fs.rmSync(outDir, { recursive: true, force: true });
+      }
+      fs.mkdirSync(outDir);
+
+      const sourceFilename = process.env.MANIFEST_V3 == 'true' ? 'v3.json' : 'v2.json'
+      const destPath = resolve(outDir, 'manifest.json')
+      fs.copyFileSync(resolve('./manifest', sourceFilename), destPath)
+      console.log(`Copied ${sourceFilename} to ${destPath}`)
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), copyManifest()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   build: {
-    emptyOutDir: true,
-    outDir: process.env.OUT_DIR || './dist/',
+    emptyOutDir: false,
+    outDir,
     rollupOptions: {
       input: {
         index: new URL('./index.html', import.meta.url).pathname,
