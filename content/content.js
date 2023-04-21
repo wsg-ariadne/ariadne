39f8c4e4ffdf667fdc083ea84a74343943689f5b
Jared Dantis <jareddantis@gmail.com>
2023-04-22 01:05:38 +0800

    content.js: Use Promisified `sendMessage()`

--- content/content.js
+++ content/content.js
@@ -87,12 +87,12 @@ document.onreadystatechange = () => {
     if (document.readyState !== "complete") { return; }
 
     // Call Report API for stats
-    (async () => await browser.runtime.sendMessage({
+    browser.runtime.sendMessage({
         action: "requestStats",
         args: { url: window.location.href }
-    }, (response) => {
+    }).then((response) => {
         console.log("[content] Stats result from Dionysus API:", response);
-    }))();
+    });
 
     // Wait for 5 sec
     console.log("[content] DOM is now ready, waiting for 5 sec before performing detection");
