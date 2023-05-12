c8d0d6a931f1beb1e2fdd2ad2199622f9c2d106b
Jared Dantis <jareddantis@gmail.com>
2023-05-12 23:33:31 +0800

    chore: Install Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite.{js,ts}",
   ],
   theme: {
     extend: {
@@ -36,5 +37,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
