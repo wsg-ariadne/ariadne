83d6422aadf5b28324aef1cc1e9fec08a1f41ee9
Jared Dantis <jareddantis@gmail.com>
2023-05-13 12:15:32 +0800

    vite: Set extension version dynamically

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import * as fs from 'node:fs'
 import { resolve } from 'node:path'
+import child_process from 'node:child_process'
 import vue from '@vitejs/plugin-vue'
 
 const outDir = process.env.MANIFEST_V3 == 'true' ? './dist-v3/' : './dist/'
@@ -14,9 +15,30 @@ const copyManifest = () => {
         fs.mkdirSync(outDir);
       }
 
-      const sourceFilename = process.env.MANIFEST_V3 == 'true' ? 'v3.json' : 'v2.json'
+      // Read source manifest
+      const sourceFilename = process.env.MANIFEST_V3 == 'true' ? 'v3' : 'v2'
+      let source = fs.readFileSync(`./manifest/${sourceFilename}.json`, 'utf8')
+
+      // Replace __PKG_version__ with package version from package.json
+      const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+      source = source.replace(/__PKG_version__/g, packageJson.version)
+
+      // Replace __PKG_versionName__ with date, git branch, and commit hash
+      const date = new Date().toISOString().slice(0, 10).replace(/-/g, '')
+      let versionName = `${date} dev`
+      try {
+        const commitHash = child_process.execSync('git rev-parse --short HEAD').toString().trim()
+        const branchName = child_process.execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+        versionName = `${date} ${branchName}@${commitHash}`
+      } catch (e) {
+        console.log('Failed to get git branch and commit hash.')
+      }
+      source = source.replace(/__PKG_versionName__/g, `${packageJson.version} (${versionName})`)
+
+      // Write manifest to build directory
       const destPath = resolve(outDir, 'manifest.json')
-      fs.copyFileSync(resolve('./manifest', sourceFilename), destPath)
+      fs.writeFileSync(destPath, source)
+
       console.log(`\nCopied ${sourceFilename} to ${destPath}\n`)
     }
   }
