2d5e688e36c4248cfd3e6982f962184558c11a44
Jared Dantis <jareddantis@gmail.com>
2023-05-13 12:33:23 +0800

    background: Delete DB on installation

--- background/background.js
+++ background/background.js
@@ -5,6 +5,17 @@ import {
   tabUrlChangeListener
 } from './listeners';
 
-browser.runtime.onMessage.addListener(messageListener);
-browser.tabs.onActivated.addListener(tabChangeListener);
-browser.tabs.onUpdated.addListener(tabUrlChangeListener);
+browser.runtime.onInstalled.addListener((details) => {
+  console.log('Extension installed', details);
+  
+  // Delete IndexedDB database on register
+  const deleteDatabase = indexedDB.deleteDatabase('ariadne');
+  deleteDatabase.onsuccess = () => {
+    console.log('Database deleted');
+
+    // Register listeners
+    browser.runtime.onMessage.addListener(messageListener);
+    browser.tabs.onActivated.addListener(tabChangeListener);
+    browser.tabs.onUpdated.addListener(tabUrlChangeListener);
+  }
+});
