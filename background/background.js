f6171c131a24353a0dced12f4419741eadf6aeb8
Jared Dantis <jareddantis@gmail.com>
2023-05-13 13:36:43 +0800

    background: Delete DB on launch

--- background/background.js
+++ background/background.js
@@ -3,21 +3,21 @@ import {
   messageListener,
   tabChangeListener,
   tabCloseListener,
-  tabUrlChangeListener
+  tabUpdateListener
 } from './listeners';
 
 browser.runtime.onInstalled.addListener((details) => {
   console.log('Extension installed', details);
-  
-  // Delete IndexedDB database on register
-  const deleteDatabase = indexedDB.deleteDatabase('ariadne');
-  deleteDatabase.onsuccess = () => {
-    console.log('Database deleted');
-
-    // Register listeners
-    browser.runtime.onMessage.addListener(messageListener);
-    browser.tabs.onActivated.addListener(tabChangeListener);
-    browser.tabs.onRemoved.addListener(tabCloseListener);
-    browser.tabs.onUpdated.addListener(tabUrlChangeListener);
-  }
 });
+  
+// Delete IndexedDB database on load
+const deleteDatabase = indexedDB.deleteDatabase('ariadne');
+deleteDatabase.onsuccess = () => {
+  console.log('Database deleted');
+
+  // Register listeners
+  browser.runtime.onMessage.addListener(messageListener);
+  browser.tabs.onActivated.addListener(tabChangeListener);
+  browser.tabs.onRemoved.addListener(tabCloseListener);
+  browser.tabs.onUpdated.addListener(tabUpdateListener);
+}
